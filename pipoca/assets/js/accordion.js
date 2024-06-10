16611877e513eed5612976e702c05ef2e122fe63
Jheimys Bassani <jheimysbb@gmail.com>
2024-06-10 22:02:55 +0100

    solving bug in the script accordion.js

--- pipoca/assets/js/accordion.js
+++ pipoca/assets/js/accordion.js
@@ -1,42 +1,26 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+// 
 
-// for (i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener("click", function() {
-//       this.classList.toggle("active");
-//       var panel = this.nextElementSibling;
-//       if (panel.style.maxHeight) {
-//         panel.style.maxHeight = null;
-//       } else {
-//         panel.style.maxHeight = panel.scrollHeight + "px";
-//       }
-//     });
-// }
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        // Fechar todos os painéis, exceto o clicado
-        for (var j = 0; j < acc.length; j++) {
-            var panel = acc[j].nextElementSibling;
-            if (acc[j] !== this) {
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            // Alternar o painel clicado
+            var panel = this.nextElementSibling;
+            var isActive = this.classList.contains("active");
+
+            // Fechar todos os outros painéis
+            for (var j = 0; j < acc.length; j++) {
+                var otherPanel = acc[j].nextElementSibling;
                 acc[j].classList.remove("active");
-                // Forçar a transição antes de definir null
-                panel.style.maxHeight = panel.scrollHeight + "px";
-                setTimeout((function(panel) {
-                    return function() {
-                        panel.style.maxHeight = null;
-                    };
-                })(panel), 10);
+                otherPanel.style.maxHeight = null;
             }
-        }
 
-        // Alternar o painel clicado
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+            // Se o painel clicado não estava ativo, ativá-lo
+            if (!isActive) {
+                this.classList.add("active");
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+});
